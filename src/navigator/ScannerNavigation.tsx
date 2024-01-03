5ae31c882958f70b3d7120c54a649db57a963862
MorisLuis <moradoluisenrique@gmail.com>
2024-01-02 21:39:30 -0600

    S / Barcode setup - get the number barcode

--- src/navigator/ScannerNavigation.tsx
+++ src/navigator/ScannerNavigation.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { Camera } from '../screens/Camera/Camera';
+import CustomCamera from '../screens/Camera/Camera';
 import { LogBox, StyleSheet } from 'react-native';
 import { Inventory } from '../screens/Camera/Inventory';
 import { AddProduct } from '../screens/Camera/AddProduct';
@@ -23,7 +23,7 @@ export const ScannerNavigation = () => {
         <Tab.Navigator
             tabBar={(props) => <CustomTabBar {...props} />}
         >
-            <Tab.Screen name="Camera" component={Camera} options={{ title: 'Camara' }} />
+            <Tab.Screen name="CustomCamera" component={CustomCamera} options={{ title: 'Camara' }} />
             <Tab.Screen name="Inventory" component={Inventory} options={{ title: 'Inventario' }} />
             <Tab.Screen name="+" component={AddProduct} options={{ title: '+' }} />
         </Tab.Navigator>
--- src/screens/Camera/Camera.tsx
+++ src/screens/Camera/Camera.tsx
@@ -1,10 +1,64 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react';
 
-export const Camera = () => {
+import { Button, View, Text } from 'react-native';
+import { Camera, useCameraDevices, useCodeScanner } from 'react-native-vision-camera';
+
+const CustomCamera: React.FC = () => {
+    const [scannedCodes, setScannedCodes] = useState<string | undefined>();
+    const [isScannerActive, setIsScannerActive] = useState(false);
+    const [selectedDevice, setSelectedDevice] = useState<string | null>(null);
+
+    const codeScanner = useCodeScanner({
+        codeTypes: ['qr', 'ean-13'],
+        onCodeScanned: (codes) => {
+            if (codes.length > 0) {
+                const scannedCode = codes[0]; // Obtener el primer código escaneado
+                const codeValue = scannedCode.value; // Suponiendo que 'value' contiene el valor del código escaneado
+
+                // Usar 'codeValue' para realizar alguna operación o mostrarlo en tu componente
+                console.log(`Scanned code value: ${codeValue}`);
+                setScannedCodes(codeValue)
+            }
+        }
+    });
+
+
+    const devices = useCameraDevices();
+    const backCamera = devices.find((device) => device.position === 'back');
+
+    const toggleScanner = () => {
+        setIsScannerActive((prev) => !prev);
+    };
+
+    const handleCameraOpen = () => {
+        setSelectedDevice(backCamera?.id || null);
+    };
+
     return (
-        <View>
-            <Text>Camara</Text>
+        <View style={{ flex: 1 }}>
+            {backCamera && (
+                <View style={{ flex: 1 }}>
+                    <Button title="Abrir cámara trasera" onPress={handleCameraOpen} />
+                    <Camera
+                        style={{ flex: 1 }}
+                        device={backCamera}
+                        isActive={selectedDevice !== null}
+                        codeScanner={isScannerActive ? codeScanner : undefined}
+                    />
+                    <Button title="Toggle Scanner" onPress={toggleScanner} />
+                    <View>
+                        {
+                            scannedCodes &&
+                            <View>
+                                <Text>Scanned Codes:</Text>
+                                <Text>{scannedCodes}</Text>
+                            </View>
+                        }
+                    </View>
+                </View>
+            )}
         </View>
-    )
-}
+    );
+};
+
+export default CustomCamera;
