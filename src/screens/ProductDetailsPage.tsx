0c1a37696516be4f97e465b1c78384852fdefcb5
Morado <moradoluisenrique@gmail.com>
2024-02-06 22:20:54 -0600

    S / Show data from ProductDetailsPage

--- src/screens/ProductDetailsPage.tsx
+++ src/screens/ProductDetailsPage.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react'
-import { Text, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View, Button } from 'react-native'
 import { getProductDetails } from '../services/products';
 import PorductInterface from '../interface/product';
 
@@ -27,13 +27,80 @@ export const ProductDetailsPage = ({ route }: any) => {
 
 
     return (
-        <TouchableOpacity onPress={navigateBackToInventario}>
-            <Text>Navegar a InventaryDetails</Text>
-            <Text>
-                {
-                    productDetails && JSON.stringify(productDetails)
-                }
-            </Text>
+        <TouchableOpacity onPress={navigateBackToInventario} style={styles.ProductDetailsPage}>
+            <View style={styles.section}>
+                <Text style={styles.title}>Descripcion: </Text>
+                <Text style={styles.data}>{productDetails?.Descripcion}</Text>
+            </View>
+            <View style={styles.section}>
+                <Text style={styles.title}>Codigo: </Text>
+                <Text style={styles.data}>{productDetails?.Codigo}</Text>
+            </View>
+            <View style={styles.section}>
+                <Text style={styles.title}>Existencia: </Text>
+                <Text style={styles.data}>{productDetails?.Existencia}</Text>
+            </View>
+            <View style={styles.section}>
+                <Text style={styles.title}>Familia: </Text>
+                <Text style={styles.data}>{productDetails?.Familia}</Text>
+            </View>
+            <View style={styles.section}>
+                <Text style={styles.title}>Marca: </Text>
+                <Text style={styles.data}>{productDetails?.Marca}</Text>
+            </View>
+            <View style={styles.section}>
+                <Text style={styles.title}>Piezas: </Text>
+                <Text>{productDetails?.Piezas || productDetails?.Existencia}</Text>
+            </View>
+            <View style={styles.section}>
+                <Text style={styles.title}>Precio: </Text>
+                <Text style={styles.data}>{productDetails?.Precio}</Text>
+            </View>
+            {
+                productDetails?.CodBar ?
+                    <View style={styles.section}>
+                        <Text style={styles.title}>Codigo de barras: </Text>
+                        <Text style={styles.data}>{productDetails?.CodBar}</Text>
+                    </View>
+                    :
+                    <View style={styles.container}>
+                        <TouchableOpacity style={styles.button}>
+                            <Text style={styles.buttonText}>Crear codigo de barras</Text>
+                        </TouchableOpacity>
+                    </View>
+            }
         </TouchableOpacity>
     )
 }
+
+const styles = StyleSheet.create({
+    ProductDetailsPage: {
+        padding: 20
+    },
+    section: {
+        marginBottom: 10,
+        display: "flex",
+        flexDirection: "row"
+    },
+    title: {
+        fontWeight: "bold"
+    },
+    data: {
+
+    },
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 20,
+    },
+    button: {
+        backgroundColor: 'blue',
+        padding: 10,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+})
