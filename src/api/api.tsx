219fa56b4bfd5725572bf54908cc6959a705fe3b
Morado <moradoluisenrique@gmail.com>
2024-02-22 11:49:43 -0600

    S / with api from backend

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-/* export const api = axios.create(
-    {
-        baseURL: 'https://olei-app-gcl8n.ondigitalocean.app'
-    }
-) */
-
-
 export const api = axios.create(
+    {
+        baseURL: 'https://seahorse-app-spuvc.ondigitalocean.app/'
+    }
+)
+
+
+/* export const api = axios.create(
     {
         baseURL: 'http://192.168.1.3:5001'
     }
-)
+) */
