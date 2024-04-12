ee531cda27c01d73278b59bdc1fc189eddb3babb
Morado <moradoluisenrique@gmail.com>
2024-04-12 17:30:00 -0600

    F / Date format deleted

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -1,16 +1,16 @@
 import { format } from 'date-fns';
-import es from 'date-fns/locale/es';
+import { es } from 'date-fns/locale';
 
 const capitalizeProperNouns = (str: string) => {
     return str.replace(/\w\S*/g, (word) => {
         if (['de', 'a', 'las'].includes(word.toLowerCase())) {
-            return word; // No capitalizar conectores
+            return word;
         }
         return word.charAt(0).toUpperCase() + word.slice(1);
     });
 }
-export const dateFormat = (date: string | undefined ) => {
 
+export const dateFormat = (date: string | undefined ) => {
     if(!date) return;
 
     const formattedDate = format(new Date(date), "EEEE, d 'de' MMMM 'de' y", { locale: es });
