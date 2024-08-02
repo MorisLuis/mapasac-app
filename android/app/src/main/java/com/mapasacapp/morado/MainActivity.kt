1c0dfd200e7c7a276306a0ac79ad7ea78f1ad911
“MorisLuis” <moradoluisenrique@gmail.com>
2024-08-02 12:57:19 -0600

    S / Settings nedded to upload on play store

--- android/app/src/main/java/com/mapasacapp/morado/MainActivity.kt
+++ android/app/src/main/java/com/mapasacapp/morado/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.oleisoftware.oleiapp
+package com.mapasacapp.morado
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/mapasacapp/morado/MainApplication.kt
+++ android/app/src/main/java/com/mapasacapp/morado/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.oleisoftware.oleiapp
+package com.mapasacapp.morado
 
 import android.app.Application
 import com.facebook.react.PackageList
