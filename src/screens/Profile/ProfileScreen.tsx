d72ceed9e598414c9753f6c4b09e7571202b437d
“MorisLuis” <moradoluisenrique@gmail.com>
2024-10-10 15:39:25 -0600

    S / Modal close error

--- src/screens/Profile/ProfileScreen.tsx
+++ src/screens/Profile/ProfileScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 
 import { SafeAreaView, TouchableOpacity, View } from 'react-native'
 import { useNavigation } from '@react-navigation/native';
@@ -12,6 +12,9 @@ import { InventoryBagContext } from '../../context/Inventory/InventoryBagContext
 import CustomText from '../../components/Ui/CustumText';
 import ButtonCustum from '../../components/Inputs/ButtonCustum';
 import { ProfileNavigationProp } from '../../interface/navigation';
+import ModalDecision from '../../components/Modals/ModalDecision';
+import { globalStyles } from '../../theme/appTheme';
+import useErrorHandler from '../../hooks/useErrorHandler';
 
 
 export const ProfileScreen = () => {
@@ -19,61 +22,91 @@ export const ProfileScreen = () => {
     const { logOut } = useContext(AuthContext);
     const { handleCleanState } = useContext(SellsBagContext);
     const { handleCleanState: handleCleanStateInventory } = useContext(InventoryBagContext);
+    const [openModalDecision, setOpenModalDecision] = useState(false);
+    const [logingOut, setlogingOut] = useState(false)
 
     const version = DeviceInfo.getVersion(); // Esto obtiene la versión de la aplicación
 
     const { theme, typeTheme } = useTheme();
     const { navigate } = useNavigation<ProfileNavigationProp>();
+    const { handleError } = useErrorHandler()
 
     const iconColor = typeTheme === 'dark' ? "white" : "black";
 
     const handleLogOut = async () => {
-        await logOut()
-        handleCleanState()
-        handleCleanStateInventory()
-    }
+        try {
+            setlogingOut(true)
+            await logOut()
+            handleCleanState()
+            handleCleanStateInventory()
+        } catch (error) {
+            handleError(error);
+        } finally {
+            setlogingOut(false)
+        }
+    };
 
     return (
-        <SafeAreaView style={{ backgroundColor: theme.background_color, flex: 1 }} >
-            <View style={ProfileScreenStyles(theme, typeTheme).ProfileScreen}>
-                <CustomText style={ProfileScreenStyles(theme, typeTheme).title}>Configuación</CustomText>
+        <>
+            <SafeAreaView style={{ backgroundColor: theme.background_color, flex: 1 }} >
+                <View style={ProfileScreenStyles(theme, typeTheme).ProfileScreen}>
+                    <CustomText style={ProfileScreenStyles(theme, typeTheme).title}>Configuación</CustomText>
 
-                <TouchableOpacity onPress={() => navigate('[ProfileNavigation] - personalInformationScreen')} style={ProfileScreenStyles(theme, typeTheme).section}>
-                    <CustomText style={{ color: theme.text_color }}>Información Personal</CustomText>
-                    <Icon name="person-outline" size={22} color={iconColor} />
-                </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigate('[ProfileNavigation] - personalInformationScreen')} style={ProfileScreenStyles(theme, typeTheme).section}>
+                        <CustomText style={{ color: theme.text_color }}>Información Personal</CustomText>
+                        <Icon name="person-outline" size={22} color={iconColor} />
+                    </TouchableOpacity>
 
-                <View style={ProfileScreenStyles(theme, typeTheme).divider}></View>
+                    <View style={ProfileScreenStyles(theme, typeTheme).divider}></View>
 
 
-                <TouchableOpacity onPress={() => navigate('[ProfileNavigation] - settingsSceen')} style={[ProfileScreenStyles(theme, typeTheme).section]}>
-                    <CustomText style={{ color: theme.text_color }}>Configuración General</CustomText>
-                    <Icon name="settings-outline" size={22} color={iconColor} />
-                </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigate('[ProfileNavigation] - settingsSceen')} style={[ProfileScreenStyles(theme, typeTheme).section]}>
+                        <CustomText style={{ color: theme.text_color }}>Configuración General</CustomText>
+                        <Icon name="settings-outline" size={22} color={iconColor} />
+                    </TouchableOpacity>
 
-                <View style={ProfileScreenStyles(theme, typeTheme).divider}></View>
+                    <View style={ProfileScreenStyles(theme, typeTheme).divider}></View>
 
-                <CustomText style={ProfileScreenStyles(theme, typeTheme).title}>Legal</CustomText>
+                    <CustomText style={ProfileScreenStyles(theme, typeTheme).title}>Legal</CustomText>
 
-                <TouchableOpacity onPress={() => navigate('[ProfileNavigation] - privacyScreen')} style={[ProfileScreenStyles(theme, typeTheme).section]}>
-                    <CustomText style={{ color: theme.text_color }}>Aviso de privacidad</CustomText>
-                    <Icon name="book-outline" size={22} color={iconColor} />
-                </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigate('[ProfileNavigation] - privacyScreen')} style={[ProfileScreenStyles(theme, typeTheme).section]}>
+                        <CustomText style={{ color: theme.text_color }}>Aviso de privacidad</CustomText>
+                        <Icon name="book-outline" size={22} color={iconColor} />
+                    </TouchableOpacity>
 
-                <View style={ProfileScreenStyles(theme, typeTheme).divider}></View>
+                    <View style={ProfileScreenStyles(theme, typeTheme).divider}></View>
 
-                <View style={ProfileScreenStyles(theme, typeTheme).closeSession}>
-                    <ButtonCustum
-                        title="Cerrar sesión"
-                        onPress={handleLogOut}
-                    />
+                    <View style={ProfileScreenStyles(theme, typeTheme).closeSession}>
+                        <ButtonCustum
+                            title="Cerrar sesión"
+                            onPress={() => setOpenModalDecision(true)}
+                        />
+                    </View>
+
+                    <View>
+                        <CustomText style={{ color: theme.text_color }}>Version: {version}</CustomText>
+                    </View>
+
                 </View>
+            </SafeAreaView>
 
-                <View>
-                    <CustomText style={{ color: theme.text_color }}>Version: {version}</CustomText>
-                </View>
-
-            </View>
-        </SafeAreaView>
+            <ModalDecision
+                visible={openModalDecision}
+                message="Seguro de cerrar sesión"
+            >
+                <ButtonCustum
+                    title="Cerrar sesión"
+                    onPress={handleLogOut}
+                    extraStyles={{ ...globalStyles(theme).globalMarginBottomSmall }}
+                    disabled={logingOut}
+                />
+                <ButtonCustum
+                    title="Cancelar"
+                    onPress={() => setOpenModalDecision(false)}
+                    buttonColor="white"
+                    disabled={logingOut}
+                />
+            </ModalDecision>
+        </>
     )
 }
